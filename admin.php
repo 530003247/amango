8a2371c02a8214a849c8076c03631b3a6d55772f
amango <530003247@qq.com>
2014-10-24 13:42:44 +0800

    基础入口

---
+++ admin.php
@@ -0,0 +1,13 @@
+<?php
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+define ( 'HEAD_URL', 'http://'.$_SERVER['SERVER_NAME'] );
+define('AMANGO_FILE_ROOT',str_replace('\\','/',realpath(dirname(__FILE__).'/'))."/");
+define('APP_DEBUG', true );
+$_GET['m'] = 'Admin';
+define ( 'APP_PATH', './Application/' );
+if(!is_file(APP_PATH . 'User/Conf/config.php')){
+	header('Location: ./install.php');
+	exit;
+}
+define ( 'RUNTIME_PATH', './Runtime/' );
+require './ThinkPHP/ThinkPHP.php';
