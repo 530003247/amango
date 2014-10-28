fd3ecf09a62480d4533946643b2185d8e0804d54
amango <530003247@qq.com>
2014-10-28 18:15:06 +0800

    微信主控制器移除 Rout，类型请求提前

--- Application/Weixin/Controller/IndexController.class.php
+++ Application/Weixin/Controller/IndexController.class.php
@@ -19,30 +19,23 @@ class IndexController{
 
 		public function index(){
 			//判断是否存在模式锁定
+			global $_W;
+			$event       = empty($_W['event'])?self::discrevent($_W['msgtype']):$_W['event'];
+			//新增芒果自定义参数 user_post
+			$_W['post']  = ucfirst(strtolower($event));
+
 			$lastmodel = get_line_model();
 			$crosstime = time()-$lastmodel[1];
 			if(!empty($lastmodel[0])&&$crosstime<=300){
 				$addonparam = explode('/', $lastmodel[0]);
 				Amango_Addons($addonparam[0],'',$addonparam[1],'',fasle);
+			} else {
+	            $Bundleclass = "Weixin\Bundle\\".$_W['post']."Bundle";
+	            $Bundle      = new $Bundleclass();
+	            $Bundle->run();
 			}
-			self::AmangoRoute();
-		}      
-
-		public function AmangoRoute(){
-			//普通事件  无event 还要具体划分 text image voice video location link
-			//订阅事件   subscribe 取消 unsubscribe
-			//扫描二维码 scan [已关注] subscribe[未关注]
-			//地理位置   location
-			//自定义菜单 click
-			global $_W;
-			$event       = empty($_W['event'])?self::discrevent($_W['msgtype']):$_W['event'];
-			//新增芒果自定义参数 user_post
-			$_W['post']  = ucfirst(strtolower($event));
-            $Bundleclass = "Weixin\Bundle\\".$_W['post']."Bundle";
-            $Bundle      = new $Bundleclass();
-            $factoryxml  = $Bundle->run();
-            wx_error($factoryxml);
 		}
+
 		protected function discrevent($event){
 			//TODO 此处 若腾讯新出接口  可以增加数组新元素
             if(in_array($event, $this->event_safe)){
